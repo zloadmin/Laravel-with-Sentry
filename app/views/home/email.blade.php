d20ee2466de5785673353ae2478480c6a77ba7aa
zloadmin <ded@xakep.ru>
2015-01-21 09:41:42 +0500

    Second commit

--- app/views/home/email.blade.php
+++ app/views/home/email.blade.php
@@ -1,12 +1,12 @@
 <html>
    <head>
-      <title>Password Reset</title>
+      <title>Востановление пароля</title>
    </head>
    <body>
-    <p>Hi {{$name}}!</p>
+    <p>Привет {{$name}}!</p>
     <br>
-      <p>Someone requested password request for {{$email}}. If it was not you please ignore this email.</p>
-    <p>To reset your password click on the following link : {{$recoverUrl}}</p>
+      {{--<p>Someone requested password request for {{$email}}. If it was not you please ignore this email.</p>--}}
+    <p>Для востановления пароля пройдите по ссылке : {{$recoverUrl}}</p>
     <br>
     <p>Your App Support Team</p>
    </body>
